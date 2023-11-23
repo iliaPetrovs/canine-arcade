63e2bbedfd7db37798e1312fae6839e09b655d66
Ilja Petrovs <ilja.petrovs@transferwise.com>
2023-11-23 01:20:40 +0000

    panels mobile fix

--- app/subcomponents/Panel/Panel.tsx
+++ app/subcomponents/Panel/Panel.tsx
@@ -5,6 +5,10 @@
 import classNames from 'classnames';
 import styles from './Panel.module.css';
 import Arrow from '../Arrow/Arrow';
+import {useEffect, useState} from 'react';
+import {useNavigate} from '@remix-run/react';
+
+const mobileCutoff = 967;
 
 type PanelProps = {
   collection: {
@@ -24,6 +28,9 @@ const Panel = ({
   index,
   currentIdx,
 }: PanelProps) => {
+  const [isMobile, setIsMobile] = useState(false);
+  const navigate = useNavigate();
+
   const getArrowPos = () => {
     if (index === 0 || (currentIdx === 2 && index === 1)) {
       return ['left'];
@@ -35,17 +42,33 @@ const Panel = ({
 
     return ['right'];
   };
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth < mobileCutoff);
+    window.addEventListener('resize', (event: Event) => {
+      setIsMobile((event.target as Window).innerWidth < mobileCutoff);
+    });
+
+    window.removeEventListener('resize', () => {});
+  }, []);
+
+  const handleContainerClick = () => {
+    if (isActive || isMobile) {
+      navigate(`/pages/${name.toLowerCase()}`);
+    }
+    handleClick(index);
+  };
   return (
     <div
       className={classNames(styles.panel, {
         [styles.active]: isActive,
       })}
-      onClick={() => handleClick(index)}
+      onClick={() => handleContainerClick()}
     >
       <h3 className={classNames({[styles.hidden]: !isActive})}>{name}</h3>
       <img src={href} alt="" />
       <div className={styles.cover} />
-      {!isActive && (
+      {!isActive && !isMobile && (
         <div
           className={classNames(styles.arrowWrapper, {
             [styles.right]: index === 0 || (index === 1 && currentIdx === 2),
@@ -55,6 +78,17 @@ const Panel = ({
           <Arrow position={getArrowPos()} />
         </div>
       )}
+      {/* TODO later
+      {isMobile && (
+        <>
+          <div className={classNames(styles.arrowWrapper, styles.right)}>
+            <Arrow position={['left']} />
+          </div>
+          <div className={classNames(styles.arrowWrapper, styles.left)}>
+            <Arrow position={['right']} />
+          </div>
+        </>
+      )} */}
     </div>
   );
 };
