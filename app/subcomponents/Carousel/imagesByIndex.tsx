007e3687450e70a9e3ffba72429a0142f5013ba4
Ilja Petrovs <ilja.petrovs@transferwise.com>
2023-11-28 19:02:55 +0000

    images

--- app/subcomponents/Carousel/imagesByIndex.tsx
+++ app/subcomponents/Carousel/imagesByIndex.tsx
@@ -3,9 +3,9 @@ import image2 from './image/bags.png';
 import image3 from './image/clothing.png';
 
 export const images: Record<string, string>[] = [
-  {image: image1, href: '/collections/clothing'},
+  {image: image1, href: '/collections/accessories-makeup'},
   {image: image2, href: '/collections/bags'},
-  {image: image3, href: '/collections/accessories-makeup'},
+  {image: image3, href: '/collections/clothing'},
 ];
 
 export const imageByIndex = (index: number): string =>
