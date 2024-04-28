a9ddadd0434d9e7fe52f11d5e1ab01950238b8f7
Ilja Petrovs <ilja.petrovs@transferwise.com>
2024-04-28 22:37:00 +0100

    remove testing array

--- app/subcomponents/Fursuits/Fursuits.tsx
+++ app/subcomponents/Fursuits/Fursuits.tsx
@@ -20,30 +20,28 @@ type FursuitsProps = {
 const Fursuits = ({fursuits}: FursuitsProps) => {
   return (
     <div className="row">
-      {[...fursuits, ...fursuits, ...fursuits, ...fursuits].map(
-        (fursuit, index) => (
-          <div
-            key={fursuit.id}
-            className={classNames('col-xs-12 col-lg-5 text-center m-b-5', {
-              'col-lg-offset-1': index % 2 === 0,
-            })}
-          >
-            <div key={fursuit.id} className={styles.container}>
-              <div className={styles.wrapper}>
-                <div className="image-container">
-                  <img
-                    className={styles.image}
-                    src={fursuit.featuredImage?.url}
-                    alt={fursuit.featuredImage?.altText || fursuit.title}
-                  />
-                </div>
-                <h2 className="header-2 capitalize m-b-0">{fursuit.title}</h2>
-                <p className={styles.description}>{fursuit.description}</p>
+      {fursuits.map((fursuit, index) => (
+        <div
+          key={fursuit.id}
+          className={classNames('col-xs-12 col-lg-5 text-center m-b-5', {
+            'col-lg-offset-1': index % 2 === 0,
+          })}
+        >
+          <div key={fursuit.id} className={styles.container}>
+            <div className={styles.wrapper}>
+              <div className="image-container">
+                <img
+                  className={styles.image}
+                  src={fursuit.featuredImage?.url}
+                  alt={fursuit.featuredImage?.altText || fursuit.title}
+                />
               </div>
+              <h2 className="header-2 capitalize m-b-0">{fursuit.title}</h2>
+              <p className={styles.description}>{fursuit.description}</p>
             </div>
           </div>
-        ),
-      )}
+        </div>
+      ))}
     </div>
   );
 };
