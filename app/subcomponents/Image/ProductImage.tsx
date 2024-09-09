18f60106f93b740abb5fc05f97f86f0e2be71837
Ilja Petrovs <ilja.petrovs@transferwise.com>
2024-09-09 08:17:25 +0100

    add skeleton image loading

--- app/subcomponents/Image/ProductImage.tsx
+++ app/subcomponents/Image/ProductImage.tsx
@@ -1,13 +1,34 @@
+import {useState} from 'react';
 import styles from './ProductImage.module.css';
+import classNames from 'classnames';
 
 type ProductImageProps = {
   altText?: string;
   href: string;
 };
 const ProductImage = ({altText, href}: ProductImageProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  const handleImageError = () => {
+    setIsLoading(false);
+  };
   return (
     <div className={styles.wrapper}>
-      <img alt={altText} src={href} />
+      <div className={styles.imageContainer}>
+        {isLoading && <div className={styles.skeletonImage} />}
+        <img
+          alt={altText}
+          src={href}
+          loading="lazy"
+          onLoad={handleImageLoad}
+          onError={handleImageError}
+          className={classNames({[styles.imageLoading]: isLoading})}
+        />
+      </div>
     </div>
   );
 };
