f83371de1879529f2fbfcc20f8c43e326d418b07
Ilja Petrovs <ilja.petrovs@transferwise.com>
2024-09-28 16:42:46 +0100

    redo cart and header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 import {Await, NavLink} from '@remix-run/react';
-import {Suspense} from 'react';
+import React, {Suspense} from 'react';
 import type {HeaderQuery} from 'storefrontapi.generated';
 import type {LayoutProps} from './Layout';
 import {useRootLoaderData} from '~/root';
@@ -7,8 +7,12 @@ import styles from './Header.module.css';
 import classNames from 'classnames';
 import Dropdown from '~/subcomponents/Dropdown/Dropdown';
 import banner from './img/banner.png';
-import {CiShoppingCart, CiSearch} from 'react-icons/ci';
+import {CiShoppingCart, CiSearch, CiGlass} from 'react-icons/ci';
+import {IoCloseOutline} from 'react-icons/io5';
+import {GiHamburgerMenu} from 'react-icons/gi';
 import Banner from './Banner';
+import Cart from '~/routes/cart';
+import type {MenuItem} from '@shopify/hydrogen/storefront-api-types';
 
 type HeaderProps = Pick<LayoutProps, 'header' | 'cart' | 'isLoggedIn'>;
 
@@ -16,64 +20,35 @@ type Viewport = 'desktop' | 'mobile';
 
 export function Header({header, isLoggedIn, cart}: HeaderProps) {
   const {shop, menu} = header;
-  return (
-    <>
-      <Banner />
-      <header className={classNames('header', styles.header)}>
-        {/* <div className={styles.daddyStripe}>
-          <div className={styles.stripe} />
-          <div className={styles.stripe} />
-          <div className={styles.stripe} />
-          <div className={styles.stripe} />
-          <div className={styles.stripe} />
-          <div className={styles.stripe} />
-        </div> */}
-        <NavLink
-          className={styles.headerLogo}
-          prefetch="intent"
-          to="/"
-          style={activeLinkStyle}
-          end
-        >
-          <img
-            className={styles.banner}
-            src={banner}
-            alt="Hyena cartoon holding a sewing needle with a thread coming out, underlining the words 'Canine Arcade'"
-          />
-        </NavLink>
-        <div className={styles.navbar}>
-          <HeaderMenu
-            menu={menu}
-            viewport="desktop"
-            primaryDomainUrl={header.shop.primaryDomain.url}
-          />
-        </div>
-        <div className={classNames(styles.navbar, styles.headerCta)}>
-          <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
-        </div>
-      </header>
-    </>
-  );
-}
-
-export function HeaderMenu({
-  menu,
-  primaryDomainUrl,
-  viewport,
-}: {
-  menu: HeaderProps['header']['menu'];
-  primaryDomainUrl: HeaderQuery['shop']['primaryDomain']['url'];
-  viewport: Viewport;
-}) {
+  const [isMobileNavOpen, setIsMobileNavOpen] = React.useState(false);
+  const [isCartOpen, setIsCartOpen] = React.useState(false);
   const {publicStoreDomain} = useRootLoaderData();
-  const className = `header-menu-${viewport}`;
+  const mobileNavContentRef = React.useRef<HTMLDivElement>(null);
+  const cartContentRef = React.useRef<HTMLDivElement>(null);
 
-  function closeAside(event: React.MouseEvent<HTMLAnchorElement>) {
-    if (viewport === 'mobile') {
-      event.preventDefault();
-      window.location.href = event.currentTarget.href;
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen((cur) => !cur);
+  };
+
+  const closeMobileNav = () => {
+    setIsMobileNavOpen(false);
+  };
+
+  const toggleCart = () => {
+    setIsCartOpen((cur) => !cur);
+  };
+
+  const closeCart = () => {
+    setIsCartOpen(false);
+  };
+
+  React.useEffect(() => {
+    if (isMobileNavOpen || isCartOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
     }
-  }
+  }, [isMobileNavOpen, isCartOpen]);
 
   const fursuitMenu = {
     id: 'gid://shopify/MenuItem/461609500728',
@@ -112,30 +87,243 @@ export function HeaderMenu({
       },
     ],
   };
+  const primaryDomainUrl = header.shop.primaryDomain.url;
 
   const newMenu = {
     ...menu,
-    // eslint-disable-next-line no-unsafe-optional-chaining
-    items: [...menu?.items, fursuitMenu],
+    items: [...(menu?.items || []), fursuitMenu],
   };
+
+  return (
+    <>
+      <Banner />
+      <header className={classNames('header', styles.header)}>
+        {/* <div className={styles.daddyStripe}>
+          <div className={styles.stripe} />
+          <div className={styles.stripe} />
+          <div className={styles.stripe} />
+          <div className={styles.stripe} />
+          <div className={styles.stripe} />
+          <div className={styles.stripe} />
+        </div> */}
+        <NavLink
+          className={styles.headerLogo}
+          prefetch="intent"
+          to="/"
+          style={activeLinkStyle}
+          end
+        >
+          <img
+            className={styles.banner}
+            src={banner}
+            alt="Hyena cartoon holding a sewing needle with a thread coming out, underlining the words 'Canine Arcade'"
+          />
+        </NavLink>
+        <div className={styles.navbar}>
+          <HeaderMenu
+            menu={newMenu}
+            viewport="desktop"
+            primaryDomainUrl={primaryDomainUrl}
+            publicStoreDomain={publicStoreDomain}
+          />
+        </div>
+        <div className={styles.cartDesktop}>
+          <CartToggle toggleCart={toggleCart} cart={cart} />
+        </div>
+        <div className={classNames(styles.navbar, styles.headerCta)}>
+          <HeaderCtas
+            isLoggedIn={isLoggedIn}
+            cart={cart}
+            toggleOpen={toggleMobileNav}
+            toggleCart={toggleCart}
+          />
+        </div>
+        <MobileNav
+          isMobileNavOpen={isMobileNavOpen}
+          toggleMobileNav={toggleMobileNav}
+          mobileNavContentRef={mobileNavContentRef}
+          closeMobileNav={closeMobileNav}
+          newMenu={newMenu}
+          publicStoreDomain={publicStoreDomain}
+          primaryDomainUrl={primaryDomainUrl}
+        />
+        <HeaderCart
+          cartContentRef={cartContentRef}
+          closeCart={closeCart}
+          isCartOpen={isCartOpen}
+        />
+      </header>
+    </>
+  );
+}
+
+function MobileNav({
+  isMobileNavOpen,
+  toggleMobileNav,
+  mobileNavContentRef,
+  closeMobileNav,
+  newMenu,
+  publicStoreDomain,
+  primaryDomainUrl,
+}: {
+  isMobileNavOpen: boolean;
+  toggleMobileNav: () => void;
+  mobileNavContentRef: React.RefObject<HTMLDivElement>;
+  closeMobileNav: () => void;
+  newMenu: HeaderProps['header']['menu'];
+  publicStoreDomain: string;
+  primaryDomainUrl: string;
+}) {
+  React.useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 967) {
+        closeMobileNav();
+      }
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        mobileNavContentRef.current &&
+        !mobileNavContentRef.current.contains(event.target as Node)
+      ) {
+        closeMobileNav();
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      className={classNames(styles.mobileNav, {
+        [styles.open]: isMobileNavOpen,
+      })}
+    >
+      <div ref={mobileNavContentRef} className={styles.mobileNavContent}>
+        <div className={styles.mobileNavContentWrapper}>
+          <div className={styles.invisibleBlock} />
+          <button
+            className={classNames(styles.mobileNavHamburger, {
+              [styles.open]: !isMobileNavOpen,
+            })}
+            onClick={toggleMobileNav}
+          >
+            <IoCloseOutline />
+          </button>
+          {/* Add your mobile navigation links here */}
+          <nav className={styles.mobileNavLinksWrapper}>
+            <NavLink end prefetch="intent" style={activeLinkStyle} to="/">
+              Home
+            </NavLink>
+            {(newMenu || FALLBACK_HEADER_MENU).items.map((item) => {
+              if (!item.url) return null;
+
+              // if the url is internal, we strip the domain
+              const url =
+                item.url.includes('myshopify.com') ||
+                item.url.includes(publicStoreDomain) ||
+                item.url.includes(primaryDomainUrl)
+                  ? new URL(item.url).pathname
+                  : item.url;
+              return !item.items.length ? (
+                <NavLink
+                  className={styles.mobileNavLink}
+                  end
+                  key={item.id}
+                  prefetch="intent"
+                  style={activeLinkStyle}
+                  to={url}
+                  onClick={closeMobileNav}
+                >
+                  {item.title}
+                </NavLink>
+              ) : (
+                <Dropdown
+                  item={item}
+                  publicStoreDomain={publicStoreDomain}
+                  primaryDomainUrl={primaryDomainUrl}
+                />
+              );
+            })}
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function HeaderCart({
+  isCartOpen,
+  closeCart,
+  cartContentRef,
+}: {
+  isCartOpen: boolean;
+  closeCart: () => void;
+  cartContentRef: React.RefObject<HTMLDivElement>;
+}) {
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        cartContentRef.current &&
+        !cartContentRef.current.contains(event.target as Node)
+      ) {
+        closeCart();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      className={classNames(styles.cart, {
+        [styles.open]: isCartOpen,
+      })}
+    >
+      <div ref={cartContentRef} className={styles.cartContent}>
+        <div className={styles.cartContentWrapper}>
+          <button
+            className={classNames(styles.cartButton, {
+              [styles.open]: !isCartOpen,
+            })}
+            onClick={closeCart}
+          >
+            <IoCloseOutline />
+          </button>
+          <Cart />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function HeaderMenu({
+  menu,
+  primaryDomainUrl,
+  viewport,
+  publicStoreDomain,
+}: {
+  menu: HeaderProps['header']['menu'];
+  primaryDomainUrl: HeaderQuery['shop']['primaryDomain']['url'];
+  viewport: Viewport;
+  publicStoreDomain: string;
+}) {
+  const className = `header-menu-${viewport}`;
 
   return (
     <nav
       className={classNames(className, styles.navbarProduct)}
       role="navigation"
     >
-      {viewport === 'mobile' && (
-        <NavLink
-          end
-          onClick={closeAside}
-          prefetch="intent"
-          style={activeLinkStyle}
-          to="/"
-        >
-          Home
-        </NavLink>
-      )}
-      {(newMenu || FALLBACK_HEADER_MENU).items.map((item) => {
+      {(menu || FALLBACK_HEADER_MENU).items.map((item) => {
         if (!item.url) return null;
 
         // if the url is internal, we strip the domain
@@ -150,7 +338,6 @@ export function HeaderMenu({
             className="header-menu-item"
             end
             key={item.id}
-            onClick={closeAside}
             prefetch="intent"
             style={activeLinkStyle}
             to={url}
@@ -172,24 +359,23 @@ export function HeaderMenu({
 function HeaderCtas({
   isLoggedIn,
   cart,
-}: Pick<HeaderProps, 'isLoggedIn' | 'cart'>) {
+  toggleOpen,
+  toggleCart,
+}: Pick<HeaderProps, 'isLoggedIn' | 'cart'> & {
+  toggleOpen?: () => void;
+  toggleCart?: () => void;
+}) {
   return (
-    <nav className="header-ctas" role="navigation">
+    <nav className={styles.headerCtas} role="navigation">
       {/* <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
         {isLoggedIn ? 'Account' : 'Sign in'}
       </NavLink> */}
       {/* <SearchToggle /> */}
-      <CartToggle cart={cart} />
-      <HeaderMenuMobileToggle />
+      <CartToggle toggleCart={toggleCart} cart={cart} />
+      <button onClick={toggleOpen} className={styles.mobileNavHamburgerClosed}>
+        <GiHamburgerMenu />
+      </button>
     </nav>
-  );
-}
-
-function HeaderMenuMobileToggle() {
-  return (
-    <a className="header-menu-mobile-toggle" href="#mobile-menu-aside">
-      <h3>☰</h3>
-    </a>
   );
 }
 
@@ -201,21 +387,35 @@ function SearchToggle() {
   );
 }
 
-function CartBadge({count}: {count: number}) {
+function CartBadge({
+  count,
+  toggleCart,
+}: {
+  count: number;
+  toggleCart?: () => void;
+}) {
   return (
-    <a className="cart-icon" href="#cart-aside">
+    <button onClick={toggleCart} className={styles.toggleCartButton}>
       <CiShoppingCart /> <span className="cart-count">{count}</span>
-    </a>
+    </button>
   );
 }
 
-function CartToggle({cart}: Pick<HeaderProps, 'cart'>) {
+function CartToggle({
+  cart,
+  toggleCart,
+}: Pick<HeaderProps, 'cart'> & {toggleCart?: () => void}) {
   return (
-    <Suspense fallback={<CartBadge count={0} />}>
+    <Suspense fallback={<CartBadge toggleCart={toggleCart} count={0} />}>
       <Await resolve={cart}>
         {(cart) => {
-          if (!cart) return <CartBadge count={0} />;
-          return <CartBadge count={cart.totalQuantity || 0} />;
+          if (!cart) return <CartBadge toggleCart={toggleCart} count={0} />;
+          return (
+            <CartBadge
+              toggleCart={toggleCart}
+              count={cart.totalQuantity || 0}
+            />
+          );
         }}
       </Await>
     </Suspense>
